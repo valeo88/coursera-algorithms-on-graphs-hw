dc4aaa7b2b94bf689c4bbab6aa3f7bb280ed840c
Valentin Popov <valeo.popov@gmail.com>
2018-07-12 11:57:43 +0300

    connected components is implemented

--- 09_graph_decomposition_starter_files_1/connected_components/connected_components.cpp
+++ 09_graph_decomposition_starter_files_1/connected_components/connected_components.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 using std::pair;
 
+void explore(vector<bool>& visited, vector<int>& cc_num, const vector<vector<int>>& adj, int v, int cc) {
+    visited[v] = true;
+    cc_num[v] = cc;
+    for (int w : adj[v]) {
+        if (!visited[w]) {
+            explore(visited, cc_num, adj, w, cc);
+        }
+    }
+}
+
 int number_of_components(vector<vector<int> > &adj) {
-  int res = 0;
-  //write your code here
-  return res;
+  vector<bool> visited(adj.size(), false);
+  vector<int> cc_num(adj.size(), 0);
+  int cc = 1;
+  for (int v = 0; v < adj.size(); ++v) {
+    if (!visited[v]) {
+        explore(visited, cc_num, adj, v, cc);
+        cc++;
+    }
+  }
+  auto max_it = std::max_element(cc_num.begin(), cc_num.end());
+  return max_it!=cc_num.end() ? *max_it : 0;
 }
 
 int main() {
