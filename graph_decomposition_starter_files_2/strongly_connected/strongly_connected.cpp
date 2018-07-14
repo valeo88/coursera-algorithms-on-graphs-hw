453a24a4517c3362a9e1e109a84e966e5de23367
Valentin Popov <valeo.popov@gmail.com>
2018-07-14 19:37:46 +0300

    SCC is implemented

--- graph_decomposition_starter_files_2/strongly_connected/strongly_connected.cpp
+++ graph_decomposition_starter_files_2/strongly_connected/strongly_connected.cpp
@@ -5,20 +5,62 @@
 using std::vector;
 using std::pair;
 
-int number_of_strongly_connected_components(vector<vector<int> > adj) {
+void explore(vector<vector<int> > &adj, vector<bool> &visited, int x) {
+  visited[x] = true;
+  for (int w : adj[x]) {
+    if (!visited[w]) {
+        explore(adj,visited,w);
+    }
+  }
+}
+
+void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
+  //write your code here
+  used[x]++;
+  for (int w : adj[x]) {
+    if (used[w] == 0) {
+        dfs(adj,used,order,w);
+    }
+  }
+  order.push_back(x);
+}
+
+vector<int> toposort(vector<vector<int> > adj) {
+  vector<int> used(adj.size(), 0);
+  vector<int> order;
+  //write your code here
+  for (int v = 0; v < adj.size(); ++v) {
+    if (used[v] == 0)
+        dfs(adj,used,order,v);
+  }
+  std::reverse(order.begin(), order.end());
+  return order;
+}
+
+int number_of_strongly_connected_components(vector<vector<int>>& adj, vector<vector<int>>& adj_r) {
   int result = 0;
   //write your code here
+  vector<bool> visited(adj.size(), false);
+  for (int v : toposort(adj_r)) {
+    if (!visited[v]) {
+        explore(adj, visited, v);
+        result++;
+    }
+  }
+
   return result;
 }
 
 int main() {
   size_t n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
+  vector<vector<int> > adj(n, vector<int>()); // graph G
+  vector<vector<int> > adj_r(n, vector<int>()); // reverse graph Gr
   for (size_t i = 0; i < m; i++) {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
+    adj_r[y - 1].push_back(x - 1);
   }
-  std::cout << number_of_strongly_connected_components(adj);
+  std::cout << number_of_strongly_connected_components(adj, adj_r);
 }
