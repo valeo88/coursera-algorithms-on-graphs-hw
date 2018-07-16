c13f89f1ead3833441729f242e11fa21ecf825cf
Valentin Popov <valeo.popov@gmail.com>
2018-07-16 10:34:32 +0300

    bipartite is implemented

--- paths_in_graphs_starter_files_1/bipartite/bipartite.cpp
+++ paths_in_graphs_starter_files_1/bipartite/bipartite.cpp
@@ -5,9 +5,37 @@
 using std::vector;
 using std::queue;
 
+bool color(vector<vector<int> > &adj, int s) {
+  //write your code here
+  const size_t n = adj.size();
+  const size_t inf = n + 1;
+  vector<int> dist(n, inf);
+  vector<bool> white(n, false);
+  dist[s] = 0;
+  white[s] = true;
+  queue<int> q;
+  q.push(s);
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop();
+    for (int v : adj[u]) {
+        if (dist[v] == inf) {
+            q.push(v);
+            dist[v] = dist[u] + 1;
+            white[v] = !white[u];
+        } else {
+            if (white[u] == white[v]) {
+                return false;
+            }
+        }
+    }
+  }
+  return true;
+}
+
 int bipartite(vector<vector<int> > &adj) {
   //write your code here
-  return -1;
+  return color(adj, 0) ? 1 : 0;
 }
 
 int main() {
