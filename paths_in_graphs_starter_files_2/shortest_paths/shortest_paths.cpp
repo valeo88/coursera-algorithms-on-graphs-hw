37e997c9a51136a5b659f59d43997576e19004b1
Valentin Popov <valeo.popov@gmail.com>
2018-07-21 18:13:05 +0300

    Shortest path alg is implemented - slow

--- paths_in_graphs_starter_files_2/shortest_paths/shortest_paths.cpp
+++ paths_in_graphs_starter_files_2/shortest_paths/shortest_paths.cpp
@@ -10,6 +10,43 @@ using std::priority_queue;
 
 void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
   //write your code here
+  // use Bellman-Ford algorithm
+  vector<int> prev(adj.size(), -1);
+  distance[s] = 0;
+  reachable[s] = 1;
+  // repeate |V| - 1 times
+  for (int m = 0; m < adj.size() - 1; ++m) {
+      for (int s = 0; s < adj.size(); ++s) {
+        for (int j = 0; j < adj[s].size(); ++j) {
+            const int v = adj[s][j];
+            reachable[v] = 1;
+            if (distance[v] > distance[s] + cost[s][j]) {
+                distance[v] = distance[s] + cost[s][j];
+                prev[v] = s;
+            }
+        }
+      }
+  }
+  vector<int> neg_cycle_nodes;
+  // check |V|th iteration
+  for (int s = 0; s < adj.size(); ++s) {
+    for (int j = 0; j < adj[s].size(); ++j) {
+        const int v = adj[s][j];
+        if (distance[v] > distance[s] + cost[s][j]) {
+            neg_cycle_nodes.push_back(v);
+        }
+    }
+  }
+  // mark all nodes in negative cycle
+  for (int v : neg_cycle_nodes) {
+    shortest[v] = 0;
+    int x = prev[v];
+    while(x!=-1 && x!=v) {
+        shortest[x] = 0;
+        x = prev[x];
+    }
+  }
+
 }
 
 int main() {
@@ -25,7 +62,7 @@ int main() {
   }
   std::cin >> s;
   s--;
-  vector<long long> distance(n, std::numeric_limits<long long>::max());
+  vector<long long> distance(n, std::numeric_limits<int>::max());
   vector<int> reachable(n, 0);
   vector<int> shortest(n, 1);
   shortest_paths(adj, cost, s, distance, reachable, shortest);
