84e19f4949858f0ee19edfaf17c60ae75111ad9e
Valentin Popov <valeo.popov@gmail.com>
2018-07-21 14:47:17 +0300

    Djikstra's alg is implemented

--- paths_in_graphs_starter_files_2/dijkstra/dijkstra.cpp
+++ paths_in_graphs_starter_files_2/dijkstra/dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <limits>
 
 using std::vector;
 using std::queue;
@@ -9,7 +10,31 @@ using std::priority_queue;
 
 int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
   //write your code her
-  return -1;
+  const int inf = std::numeric_limits<int>::max();
+  // use long long to prevent int overflow
+  vector<long long> dist(adj.size(),inf);
+  dist[s] = 0;
+  // comparator of pairs for min-heap
+  auto compare = [](const pair<int,int>& lhs, const pair<int,int>& rhs) {
+    return lhs.second > rhs.second;
+  };
+  // first - vertex, second - dist value
+  priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(compare)> h(compare);
+  for (int i = 0; i < dist.size(); ++i) {
+    h.push(std::make_pair(i, dist[i]));
+  }
+  while (!h.empty()) {
+    pair<int,int> u = h.top();
+    h.pop();
+    for (int j = 0; j < adj[u.first].size(); ++j) {
+        const int v = adj[u.first][j];
+        if (dist[v] > dist[u.first] + cost[u.first][j]) {
+            dist[v] = dist[u.first] + cost[u.first][j];
+            h.push(std::make_pair(v, dist[v]));
+        }
+    }
+  }
+  return dist[t]==inf ? -1 : dist[t];
 }
 
 int main() {
