737a3568a28cdcc8d13abebeac788aa9048402da
Valentin Popov <valeo.popov@gmail.com>
2018-07-21 15:08:26 +0300

    Negative cycles detecting alg is implemented

--- paths_in_graphs_starter_files_2/negative_cycle/negative_cycle.cpp
+++ paths_in_graphs_starter_files_2/negative_cycle/negative_cycle.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <limits>
 
 using std::vector;
 
 int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
   //write your code here
+  // use Bellman-Ford algorithm
+  const int inf = std::numeric_limits<int>::max();
+  // use long long to prevent int overflow
+  vector<long long> dist(adj.size(),inf);
+  // let initial s = 0
+  dist[0] = 0;
+  // repeate |V| - 1 times
+  for (int m = 0; m < adj.size() - 1; ++m) {
+      for (int s = 0; s < adj.size(); ++s) {
+        for (int j = 0; j < adj[s].size(); ++j) {
+            const int v = adj[s][j];
+            if (dist[v] > dist[s] + cost[s][j]) {
+                dist[v] = dist[s] + cost[s][j];
+            }
+        }
+      }
+  }
+  // check |V|th iteration
+  for (int s = 0; s < adj.size(); ++s) {
+    for (int j = 0; j < adj[s].size(); ++j) {
+        const int v = adj[s][j];
+        if (dist[v] > dist[s] + cost[s][j]) {
+            return 1;
+        }
+    }
+  }
+
   return 0;
 }
 
